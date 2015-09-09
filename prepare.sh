6324187ad0dc4ebe3e88b798ce1eb530c8bb2c84
Andrew Meyer <ameyer@egov.com>
2015-09-09 12:41:00 -0500

    Updated prepare script.

--- prepare.sh
+++ prepare.sh
@@ -1,15 +1,15 @@
 read -p "Running this script will remove all git references to ReadyGo and will make this a 'clean' project. Proceed? " answer
 case ${answer:0:1} in
     y|Y )
-        echo 'Okay. Running commands...'
-        echo 'Installing node modules...'
+        echo 'Okay. Running commands...\n'
+        echo '\n-------\nInstalling node modules...'
         npm install
-        echo 'Removing git references...'
-        sudo rm -rf .git
-        echo 'Final cleanup: removing last files...'
+        echo '\n-------\nRemoving git references...'
+        rm -rf .git
+        echo '\n-------\nFinal cleanup: removing last files...'
         rm readme.md
         rm prepare.sh
-        echo 'Done.'
+        echo '\n-------\nDone.'
     ;;
     * )
         echo 'Alright. Not doing anything...'
